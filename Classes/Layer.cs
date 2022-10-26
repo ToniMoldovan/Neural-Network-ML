ffed7864f3d00ff334f10bf002bdfbc65f4a249d
ToniMoldovan <antonio.moldovan82@yahoo.com>
2022-10-26 23:14:48 +0300

    Need to make back-propagation

--- Classes/Layer.cs
+++ Classes/Layer.cs
@@ -32,5 +32,49 @@ namespace Proiect_3.Classes
                 }
             }
         }
+
+        private void calculateOutputValue(Neuron neuron)
+        {
+            neuron.output = neuron.activation;
+        }
+
+        private void calculateActivationValue(Neuron neuron)
+        {
+            double numarator = 1f;
+            double numitor = 1 + Math.Exp(-1 * neuron.input);
+            double act = numarator / numitor;
+
+            neuron.activation = act;
+        }
+
+        private void calculateInputValue(Neuron neuron)
+        {
+            double sum = 0;
+            for (int i = 0; i < neuron.inputValue.Length; i++)
+            {
+                sum += neuron.inputValue[i] * neuron.weightValue[i];
+            }
+            neuron.input = sum;
+        }
+
+        internal void CalculateOutput()
+        {
+            //Input
+            if (LAYER_TYPE == "input")
+            {
+                foreach (Neuron item in neurons)
+                    item.output = item.inputValue[0];
+
+            }
+            else // hidden & output
+            {
+                foreach (Neuron item in neurons)
+                {
+                    calculateInputValue(item);
+                    calculateActivationValue(item);
+                    calculateOutputValue(item);
+                }
+            }
+        }
     }
 }
--- Classes/NeuralNetwork.cs
+++ Classes/NeuralNetwork.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,15 +12,72 @@ namespace Proiect_3.Classes
         public List<Layer> layers = new List<Layer>();
         public int hiddenLayerNeurons = 30;
 
+        public double learningRate  = 0.001;
+        public double targetError   = 0.001;
+        public int epochsNumber     = 10;
+
+        public List<NormHouseData> training_normData;
+        public List<NormHouseData> testing_normData;
+
         private NeuralNetwork() {}
+
         private static NeuralNetwork Instance = null;
         public static NeuralNetwork getInstance()
         {
-            if (Instance == null) Instance = new NeuralNetwork();
+            if (Instance == null)
+            {
+                Instance = new NeuralNetwork();
+            }
 
             return Instance;
         }
 
+        public void setData(List<NormHouseData> training_normData, List<NormHouseData> testing_normData)
+        {
+            this.training_normData = training_normData;
+            this.testing_normData = testing_normData;
+        }
+
+        public void LoadDataToNetwork(NormHouseData data)
+        {
+            PropertyInfo[] properties = typeof(NormHouseData).GetProperties();
+
+            int i = 0;
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.Name == "SalePrice") break;
+
+                this.layers[0].neurons[i].inputValue[0] = (double)property.GetValue(data);
+                i++;
+            }
+
+            // Adding sale price to the target output
+            this.layers[2].neurons[0].targetOutput = data.SalePrice;
+        }
+
+        public void TransferData(Layer leftLayer, Layer rightLayer)
+        {
+            for (int i = 0; i < rightLayer.neurons.Count; i++)
+            {
+                for (int j = 0; j < rightLayer.neurons[i].inputValue.Length; j++)
+                {
+                    rightLayer.neurons[i].inputValue[j] = leftLayer.neurons[j].output;
+                }
+            }
+        }
+
+        public void FeedForward()
+        {
+            layers[0].CalculateOutput();
+
+            TransferData(layers[0], layers[1]);
+
+            layers[1].CalculateOutput();
+
+            TransferData(layers[1], layers[2]);
+
+            layers[2].CalculateOutput();
+        }
 
         public void GenerateNetwork()
         {
@@ -32,6 +90,30 @@ namespace Proiect_3.Classes
             Layer outputLayer = new Layer("output", 1, this.hiddenLayerNeurons);
             this.layers.Add(outputLayer);
         }
+        private double FindCurrentError()
+        {
+            double sum = Math.Pow((layers[2].neurons[0].targetOutput - layers[2].neurons[0].targetOutput), 2);
 
+            return sum / 2;
+        }
+
+        public void TrainNetwork()
+        {
+            List<double> error = new List<double>();
+
+            for (int i = 0; i < epochsNumber; i++)
+            {
+                foreach (NormHouseData item in training_normData)
+                {
+                    LoadDataToNetwork(item);
+                    FeedForward();
+
+                    double currentError = FindCurrentError();
+                    error.Add(currentError);
+                }
+            }
+        }
+
+        
     }
 }
